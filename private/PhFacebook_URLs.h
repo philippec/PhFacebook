c130b63c5b3eadc724e74a3ddb3f06fb32515f83
Oscar Del Ben <info@oscardelben.com>
2011-06-12 12:16:24 +0200

    merge with upstream

--- private/PhFacebook_URLs.h
+++ private/PhFacebook_URLs.h
@@ -15,13 +15,11 @@
 
 #define kFBLoginURL @"https://www.facebook.com/login.php"
 #define kFBLoginSuccessURL @"http://www.facebook.com/connect/login_success.html"
-#define kFBLoginSuccessURLHTTPS @"https://www.facebook.com/connect/login_success.html"
 
 #define kFBUIServerURL @"http://www.facebook.com/connect/uiserver.php"
-#define kFBUIServerURLHTTPS @"https://www.facebook.com/connect/uiserver.php"
 
 #define kFBAccessToken @"access_token="
-#define kFBExpiresIn   @"expires_in="
+#define kFBExpiresIn @"expires_in="
 #define kFBErrorReason @"error_description="
 
 #define kFBGraphApiGetURL @"https://graph.facebook.com/%@?access_token=%@"
